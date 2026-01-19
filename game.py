28cda76fab7c1b0e26eaff5af64c04c200c56ab5
prischantika putri <mariaprischantika@gmail.com>
2026-01-19 06:10:21 +0000

    game v1.1

--- game.py
+++ game.py
@@ -4,8 +4,10 @@ secret_number = 818
 
 gues_number = int(input("masukkan tebak angka:"))
 
+
 while gues_number != secret_number:
     print("tebakan salah, coba lagi!")
+    print("anda terjebak") #modify
     gues_number = int(input("masukkan tebak angka"))
 
 print ("selamat!!! tebakan anda benar!")
